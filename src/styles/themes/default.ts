5bf9900175dbc709f341c55555433fde0b745a8a
Paulo Santana <paulosantana.sa@gmail.com>
2023-09-20 21:16:55 -0300

    chore: setup initial project.

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,11 @@
+export const defaultTheme = {
+  white: "#fff",
+
+  "pale-blue": "#BDE5EF",
+  "navy-blue": "#2A6783",
+
+  "green-teal": "#478182",
+
+  "base-text": "#00000E",
+  "sub-text": "#BDBDBD",
+} as const;
